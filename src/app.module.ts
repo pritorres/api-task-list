7020945424b44eeff48ab48632f5cfeade015386
priscila torres <priscyla.torres16@hotmail.com>
2022-01-26 20:12:59 -0300

    practi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TaskController } from './controllers/task.controller';
 
 @Module({
   imports: [],
